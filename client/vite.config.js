39a38e1372b0344fe09a6ae42e3ec8f4eb72d74f
ZoubairMabrouk <zoubairmabrouk55@gmail.com>
2025-08-29 13:21:02 +0100

    server url

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
-      '/api': process.env.NODE_ENV === 'production'
-        ? { target: 'https://your-backend-url.com', changeOrigin: true }
-        : { target: 'http://localhost:5000', changeOrigin: true }
+      '/api': {
+        target: 'https://portfolio-backend-sand-two.vercel.app/',
+        changeOrigin: true
+      }
     }
   },
   build: {
