47c7aa8c1da1c4bdec38c297050370b8ffebfa63
ZoubairMabrouk <zoubairmabrouk55@gmail.com>
2025-09-29 21:34:36 +0100

    url

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,3 +1,3 @@
 import axios from "axios";
 
-export default Api = axios.create("https://portfolio-backend-sand-two.vercel.app/api")
+export default Api = axios.create({baseURL:"https://portfolio-backend-sand-two.vercel.app/api"})
